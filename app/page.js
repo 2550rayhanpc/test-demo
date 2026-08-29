a9532cb509ad4d0d9328c69da43e8785956dc5a1
2550rayhanpc <2550rayhanpc@gmail.com>
2026-08-28 23:52:19 -0600

    Add BackgroundAnimation component to Home page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import BackgroundAnimation from "@/components/BackgroundAnimation";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
@@ -9,6 +10,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
+      <BackgroundAnimation />
       <Navbar />
       <Hero />
       <About />
